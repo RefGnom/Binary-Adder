e5acb2b5185f146758192944657142e4cae6a10f
Cyrus <88629892+RefGnom@users.noreply.github.com>
2023-11-14 14:53:28 +0500

    Created half adder and full adder

---
+++ BinaryAdder/FullAdder.cs
@@ -0,0 +1,43 @@
+﻿namespace BinaryAdder;
+
+internal class FullAdder
+{
+    private readonly HalfAdder fromInputs = new();
+    private readonly HalfAdder fromOtherAdder = new();
+
+    public int InputA
+    {
+        set
+        {
+            fromInputs.InputA = value;
+            fromOtherAdder.InputB = fromInputs.OutputA;
+        }
+
+    }
+    public int InputB
+    {
+        set
+        {
+            fromInputs.InputB = value;
+            fromOtherAdder.InputB = fromInputs.OutputA;
+        }
+    }
+
+    public int InputFromOtherAddder
+    {
+        set
+        {
+            fromOtherAdder.InputA = value;
+        }
+    }
+
+    public int CurrentOutput => fromOtherAdder.InputA;
+    public int NextOutput => fromInputs.OutputB | fromOtherAdder.OutputB;
+
+    public void Reset()
+    {
+        InputA = 0;
+        InputB = 0;
+        InputFromOtherAddder = 0;
+    }
+}
---
+++ BinaryAdder/HalfAdder.cs
@@ -0,0 +1,16 @@
+﻿namespace BinaryAdder;
+
+internal class HalfAdder
+{
+    public int InputA { get; set; }
+    public int InputB { get; set; }
+
+    public int OutputA => InputA ^ InputB;
+    public int OutputB => InputA & InputB;
+
+    public void Reset()
+    {
+        InputA = 0;
+        InputB = 0;
+    }
+}
